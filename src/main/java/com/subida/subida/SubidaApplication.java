0f5aa57a41b8d9075fcffb8c3923efb3da43ec37
gusengxi <110712364+gusengxi@users.noreply.github.com>
2022-11-02 16:25:26 +0800

    Add files via upload

---
+++ src/main/java/com/subida/subida/SubidaApplication.java
@@ -0,0 +1,13 @@
+package com.subida.subida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubidaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/subida/subida/SubidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subida.subida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
